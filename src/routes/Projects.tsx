f60f544c1fc2e6dfdeca1bd53d49907bf4cae29f
Thasyo Peres <thasyotp2002@gmail.com>
2024-02-19 07:42:57 -0300

    making request from user repositories

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -17,15 +17,23 @@ import Error from '../components/Error'
 
 const Projects = () => {
 
-    const [projects, setProjects] = useState<ProjectsProps[]>()
+    const [projects, setProjects] = useState<ProjectsProps[]>([])
 
     const {login} = useParams()
 
     const {data, isError, isLoading} = useQueryProjects(login)
 
     useEffect(() => {
-      const projectsData: ProjectsProps[] = data
-      setProjects(projectsData)
+      if (data && Array.isArray(data)) {
+        const isValidData = data.every(item => typeof item === 'object' && item !== null);
+        if (isValidData) {
+          setProjects(data);
+        } else {
+          console.error("Data não está no formato esperado."); 
+        }
+      } else {
+        console.error("Data não é um array."); 
+      }
     }, [data])
 
   return (
@@ -35,7 +43,7 @@ const Projects = () => {
         <div>
           <button>Voltar ao início</button>
         </div>
-        {projects && projects.map(project => (
+        {projects && projects.map((project) => (
           <CardProject
             key={project.name} 
             name={project.name}
@@ -46,9 +54,9 @@ const Projects = () => {
             created_at={project.created_at}
           />
         ))}
+        {projects.length === 0 && <p>Não há repositórios!</p>}
         {isLoading && <PageLoading/>}
         {isError && <Error/>}
-        {projects?.length === 0 && <p>Não há repositórios</p>}
       </section>
     </div>
   )
